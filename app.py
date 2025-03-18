07b2982bf1398d9c5d6f8f96d4a2ab6f177f4ae8
mohak <c.mohak2004@gmail.com>
2025-03-18 12:09:13 +0530

    fix

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ import os
 from services.health_service import HealthService
 import requests
 import asyncio
-
+from fastapi.middleware.cors import CORSMiddleware
 
 # const app = express()
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 usersDB = {}
 
@@ -50,4 +58,4 @@ def get_tweet(data: TWEET):
 if __name__ == "__main__":
     # Get port from environment variable or use a default
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
